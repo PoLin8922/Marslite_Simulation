990ba15ee6a08c5dd517d667ae10113fc8582be1
berlin <berlin82289@gmail.com>
2024-08-16 21:12:51 +0800

    add yolo msgs

--- mars_ws/src/hyper_system/scripts/fuzzy_membership_function.py
+++ mars_ws/src/hyper_system/scripts/fuzzy_membership_function.py
@@ -32,49 +32,49 @@ def plot_membership_functions():
     # plt.plot(x, m_membership, 'limegreen', linewidth=3)  
     # plt.plot(x, h_membership, 'tomato', linewidth=3)  
     # # plt.plot(x, vh_membership, 'k-', linewidth=4)
-    # plt.xlabel('Semantic Level', fontsize=14)  
-    # plt.ylabel('Degree of Membership', fontsize=14)  
+    # # plt.xlabel('Semantic Level', fontsize=14)  
+    # # plt.ylabel('Degree of Membership', fontsize=14)  
     # plt.legend()
     # plt.grid(False)
-    # plt.xticks(np.arange(0, 11, 1))  
-    # plt.yticks(np.arange(0, 1.1, 0.1))  
+    # plt.xticks(np.arange(0, 11, 1), fontsize=18)  
+    # plt.yticks(np.arange(0, 1.1, 0.1), fontsize=18)  
     # plt.ylim(0, 1.1) 
     # plt.xlim(0, 10) 
     # plt.show()
 
     # # # Gausion 5
-    lb = 0
-    ub = 1
+    # lb = 0
+    # ub = 1
 
-    x = np.linspace(lb, ub, (int)(ub-lb)*1000)  
+    # x = np.linspace(lb, ub, (int)(ub-lb)*1000)  
     
-    vl_mean, vl_std_dev = lb, (ub-lb)/10
-    l_mean, l_std_dev = lb + (ub-lb)/4, (ub-lb)/10
-    m_mean, m_std_dev = lb + (ub-lb)/2, (ub-lb)/10
-    h_mean, h_std_dev = lb + (ub-lb)*3/4, (ub-lb)/10
-    vh_mean, vh_std_dev = ub, (ub-lb)/10
+    # vl_mean, vl_std_dev = lb, (ub-lb)/10
+    # l_mean, l_std_dev = lb + (ub-lb)/4, (ub-lb)/10
+    # m_mean, m_std_dev = lb + (ub-lb)/2, (ub-lb)/10
+    # h_mean, h_std_dev = lb + (ub-lb)*3/4, (ub-lb)/10
+    # vh_mean, vh_std_dev = ub, (ub-lb)/10
 
-    vl_membership = gaussian(x, vl_mean, vl_std_dev)
-    l_membership = gaussian(x, l_mean, l_std_dev)
-    m_membership = gaussian(x, m_mean, m_std_dev)
-    h_membership = gaussian(x, h_mean, h_std_dev)
-    vh_membership = gaussian(x, vh_mean, vh_std_dev)
+    # vl_membership = gaussian(x, vl_mean, vl_std_dev)
+    # l_membership = gaussian(x, l_mean, l_std_dev)
+    # m_membership = gaussian(x, m_mean, m_std_dev)
+    # h_membership = gaussian(x, h_mean, h_std_dev)
+    # vh_membership = gaussian(x, vh_mean, vh_std_dev)
 
-    plt.figure(figsize=(10, 6))
-    plt.plot(x, vl_membership, '#6A0DAD', linewidth=3)  # Purple
-    plt.plot(x, l_membership, '#1E90FF', linewidth=3)   # Deep sky blue
-    plt.plot(x, m_membership, '#32CD32', linewidth=3)   # Lime green
-    plt.plot(x, h_membership, '#FF6347', linewidth=3)   # Tomato red
-    plt.plot(x, vh_membership, '#FFD700', linewidth=3)  # Gold
-    plt.xlabel('Navigability', fontsize=14)  
-    plt.ylabel('Degree of Membership', fontsize=14)  
-    plt.legend()
-    plt.grid(False)
-    plt.xticks(np.arange(lb, ub + (ub-lb)/10, (ub-lb)/10))  
-    plt.yticks(np.arange(0, 1.1, 0.1))  
-    plt.ylim(0, 1.1) 
-    plt.xlim(lb, ub) 
-    plt.show()
+    # plt.figure(figsize=(10, 6))
+    # plt.plot(x, vl_membership, '#6A0DAD', linewidth=3)  # Purple
+    # plt.plot(x, l_membership, '#1E90FF', linewidth=3)   # Deep sky blue
+    # plt.plot(x, m_membership, '#32CD32', linewidth=3)   # Lime green
+    # plt.plot(x, h_membership, '#FF6347', linewidth=3)   # Tomato red
+    # plt.plot(x, vh_membership, '#FFD700', linewidth=3)  # Gold
+    # # plt.xlabel('Navigability', fontsize=14)  
+    # # plt.ylabel('Degree of Membership', fontsize=14)  
+    # plt.legend()
+    # plt.grid(False)
+    # plt.xticks(np.arange(lb, ub + (ub-lb)/10, (ub-lb)/10), fontsize=18)  
+    # plt.yticks(np.arange(0, 1.1, 0.1), fontsize=18)  
+    # plt.ylim(0, 1.1) 
+    # plt.xlim(lb, ub) 
+    # plt.show()
 
 
     # # Triangular
@@ -100,29 +100,29 @@ def plot_membership_functions():
     # plt.show()
 
     # trapezoidal`
-    # x = np.linspace(0, 1, 1000)  # 输入范围
+    x = np.linspace(0, 1, 1000)  # 输入范围
 
-    # # 隶属函数的参数设置
-    # s_a, s_b, s_c, s_d = 0.0, 0.0, 0.4, 0.8  # S (Small) 的参数
-    # l_a, l_b, l_c, l_d = 0.6, 0.8, 1.0, 1.0  # L (Large) 的参数
+    # 隶属函数的参数设置
+    s_a, s_b, s_c, s_d = 0.0, 0.0, 0.4, 0.8  # S (Small) 的参数
+    l_a, l_b, l_c, l_d = 0.6, 0.8, 1.0, 1.0  # L (Large) 的参数
 
-    # # 计算隶属度
-    # s_membership = trapezoidal(x, s_a, s_b, s_c, s_d)
-    # l_membership = trapezoidal(x, l_a, l_b, l_c, l_d)
+    # 计算隶属度
+    s_membership = trapezoidal(x, s_a, s_b, s_c, s_d)
+    l_membership = trapezoidal(x, l_a, l_b, l_c, l_d)
 
-    # # 绘制隶属函数
-    # plt.figure(figsize=(10, 6))
-    # plt.plot(x, s_membership, 'black', linewidth=4)
-    # plt.plot(x, l_membership, 'black', linewidth=4)
+    # 绘制隶属函数
+    plt.figure(figsize=(10, 6))
+    plt.plot(x, s_membership, '#1E90FF', linewidth=4, label='S (Small)')
+    plt.plot(x, l_membership, 'tomato', linewidth=4, label='L (Large)')
     # plt.xlabel('Prediction Mode Indicator', fontsize=14)
     # plt.ylabel('Degree of Membership', fontsize=14)
-    # # plt.legend()
-    # plt.grid(False)
-    # plt.xticks(np.arange(0, 1.1, 0.1))  
-    # plt.yticks(np.arange(0, 1.1, 0.1))  
-    # # plt.ylim(0, 1.1) 
-    # # plt.xlim(0, 1) 
-    # plt.show()
+    # plt.legend()
+    plt.grid(False)
+    plt.xticks(np.arange(0, 1.1, 0.1), fontsize=18)  
+    plt.yticks(np.arange(0, 1.1, 0.1), fontsize=18)  
+    # plt.ylim(0, 1.1) 
+    # plt.xlim(0, 1) 
+    plt.show()
 
 
     # # Linear
